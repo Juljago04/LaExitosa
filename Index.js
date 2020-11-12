0e2fd6df21acaff56383eeff130eb79164b023cc
Juljago04 <53099085+Juljago04@users.noreply.github.com>
2020-11-12 10:13:39 -0500

    Delete Index.js

--- Index.js
+++
@@ -1,8 +0,0 @@
-﻿function home() {
-    window.location.href = "Index.html";
-}
-function scrollto(div) {
-    $('html, body').animate({
-        scrollTop: $('#' + div).offset().top
-    }, 1000);
-}
